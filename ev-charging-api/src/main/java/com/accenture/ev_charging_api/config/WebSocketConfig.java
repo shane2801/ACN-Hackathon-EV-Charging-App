a2a7a484e99444993e73573f226ee06d6a0f2c2a
kavish.muthoora <kavish.muthoora@accenture.com>
2026-05-22 01:58:32 +0400

    Add WebSocket support in Spring Boot

---
+++ ev-charging-api/src/main/java/com/accenture/ev_charging_api/config/WebSocketConfig.java
@@ -0,0 +1,15 @@
+package com.accenture.ev_charging_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new OcppWebSocketHandler(), "/ocpp/{chargerId}")
+                .setAllowedOrigins("*");
+    }
+}
