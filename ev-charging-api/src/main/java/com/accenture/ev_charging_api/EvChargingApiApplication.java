10698d79936c276cdb59cda81b7a732488cbb81b
kavish.muthoora <kavish.muthoora@accenture.com>
2026-05-21 22:44:52 +0400

    Initial backend setup for EV charging API

---
+++ ev-charging-api/src/main/java/com/accenture/ev_charging_api/EvChargingApiApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.ev_charging_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvChargingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvChargingApiApplication.class, args);
+	}
+
+}
---
+++ ev-charging-api/src/test/java/com/accenture/ev_charging_api/EvChargingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.ev_charging_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvChargingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
